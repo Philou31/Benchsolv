216539666dc4fbd7cbf791e8f27ebbf7a133542e
Philou31 <leleux.philippe0@gmail.com>
2017-04-07 09:42:36 +0200

    and again

--- Metrics.cpp
+++ Metrics.cpp
@@ -1,62 +1,62 @@
-//! \file Metrics.h
-//! \brief Metrics class for the solver
-//! \author filou
-//! \version 0.1
-//! \date 22/10/2016, 18:42
-//!
-//! Class Metrics computing metrics on the problem and the found solution. This
-//! class is meant to be fair in the results gotten from the solvers: metrics 
-//! are computed in the exact same way.
-//!
-
-#include "Metrics.h"
-
-Metrics::Metrics() {
-    qrm_init_c(0);
-    dqrm_spmat_init_c(&_qrm_mat);
-}
-
-void Metrics::init_metrics(int m, int n, int nz, double *values, int *irn,
-        int*jcn, double *x, double *r) {
-    _qrm_mat.m = m;
-    _qrm_mat.n = n;
-    _qrm_mat.nz = nz;
-    _qrm_mat.val = values;
-    _qrm_mat.irn = irn;
-    _qrm_mat.jcn = jcn;
-    _x = x;
-    _r = r;
-}
-        
-Metrics::~Metrics() {    
-    qrm_finalize_c();
-}
-
-void Metrics::A_norm(double &anrm) {
-    dqrm_matnrm_c(&_qrm_mat, 'f', &anrm);
-}
-
-void Metrics::b_norm(double &bnrm) {
-    dqrm_vecnrm_c(_r, _qrm_mat.m, 1, '2', &bnrm);
-}
-
-void Metrics::sol_norm(double &xnrm) {
-    dqrm_vecnrm_c(_x, _qrm_mat.n, 1, '2', &xnrm);
-}
-
-void Metrics::residual_norm(double &rnrm) {
-    dqrm_residual_norm_c(&_qrm_mat, _r, _x, 1, &rnrm);    
-}
-
-void Metrics::residual_orth(double &onrm) {
-    dqrm_residual_orth_c(&_qrm_mat, _r, 1, &onrm);    
-}
-
-void Metrics::compute_metrics(double &rnrm, double &onrm, double &anrm, 
-        double &xnrm, double &bnrm) {
-    residual_norm(rnrm);
-    residual_orth(onrm);
-    A_norm(anrm);
-    sol_norm(xnrm);
-    b_norm(bnrm);
-}
+////! \file Metrics.h
+////! \brief Metrics class for the solver
+////! \author filou
+////! \version 0.1
+////! \date 22/10/2016, 18:42
+////!
+////! Class Metrics computing metrics on the problem and the found solution. This
+////! class is meant to be fair in the results gotten from the solvers: metrics 
+////! are computed in the exact same way.
+////!
+//
+//#include "Metrics.h"
+//
+//Metrics::Metrics() {
+//    qrm_init_c(0);
+//    dqrm_spmat_init_c(&_qrm_mat);
+//}
+//
+//void Metrics::init_metrics(int m, int n, int nz, double *values, int *irn,
+//        int*jcn, double *x, double *r) {
+//    _qrm_mat.m = m;
+//    _qrm_mat.n = n;
+//    _qrm_mat.nz = nz;
+//    _qrm_mat.val = values;
+//    _qrm_mat.irn = irn;
+//    _qrm_mat.jcn = jcn;
+//    _x = x;
+//    _r = r;
+//}
+//        
+//Metrics::~Metrics() {    
+//    qrm_finalize_c();
+//}
+//
+//void Metrics::A_norm(double &anrm) {
+//    dqrm_matnrm_c(&_qrm_mat, 'f', &anrm);
+//}
+//
+//void Metrics::b_norm(double &bnrm) {
+//    dqrm_vecnrm_c(_r, _qrm_mat.m, 1, '2', &bnrm);
+//}
+//
+//void Metrics::sol_norm(double &xnrm) {
+//    dqrm_vecnrm_c(_x, _qrm_mat.n, 1, '2', &xnrm);
+//}
+//
+//void Metrics::residual_norm(double &rnrm) {
+//    dqrm_residual_norm_c(&_qrm_mat, _r, _x, 1, &rnrm);    
+//}
+//
+//void Metrics::residual_orth(double &onrm) {
+//    dqrm_residual_orth_c(&_qrm_mat, _r, 1, &onrm);    
+//}
+//
+//void Metrics::compute_metrics(double &rnrm, double &onrm, double &anrm, 
+//        double &xnrm, double &bnrm) {
+//    residual_norm(rnrm);
+//    residual_orth(onrm);
+//    A_norm(anrm);
+//    sol_norm(xnrm);
+//    b_norm(bnrm);
+//}
--- Metrics.h
+++ Metrics.h
@@ -18,88 +18,88 @@ class Metrics {
 private:
     dqrm_spmat_type_c _qrm_mat; // QR_Mumps data structure
     double *_r, *_x;    // residual (Ax-b) and solution arrays
-public:
-    //!
-    //! \brief Constructor of the Metrics class
-    //!
-    //! \return the Metrics instance
-    //!
-    Metrics();
-    
-    //!
-    //! \brief Destructor of the Metrics class
-    //!
-    ~Metrics();
-    
-    //!
-    //! \fn void init_metrics(int m, int n, int nz, double *values, int *irn,
-    //! int*jcn, double *x, double *r)
-    //! \brief Initialize the data (QR_Mumps structure, residual and solution)
-    //!
-    //! \param m, n, nz: number of rows, columns and non-zero values
-    //! \param values, irn/jcn: arrays of non-zero values, row/column indices
-    //! \param x: solution array
-    //! \param r: residual array (Ax-b)
-    //!
-    void init_metrics(int m, int n, int nz, double *values, int *irn,
-        int*jcn, double *x, double *r);
-    
-    //!
-    //! \fn A_norm(double &anrm)
-    //! \brief Compute the norm of the matrix A
-    //!
-    //! \param anrm: norm of A
-    //!
-    void A_norm(double &anrm);
-    
-    //!
-    //! \fn b_norm(double &bnrm)
-    //! \brief Compute the norm of the right hand side b
-    //!
-    //! \param bnrm: norm of the right hand side b
-    //!
-    void b_norm(double &bnrm);
-    
-    //!
-    //! \fn sol_norm(double &xnrm)
-    //! \brief Compute the norm of the solution x
-    //!
-    //! \param xnrm: norm of the solution x
-    //!
-    void sol_norm(double &xnrm);
-    
-    //!
-    //! \fn r_norm(double &rnrm)
-    //! \brief Compute the norm of the residual r=Ax-b
-    //!
-    //! \param rnrm: norm of the residual
-    //!
-    void residual_norm(double &rnrm);
-    
-    //!
-    //! \fn o_norm(double &onrm)
-    //! \brief Compute the norm of the orthogonal residual onrm=||A'r||/||r||
-    //!
-    //! \param onrm: norm of the orthogonal residual
-    //!
-    void residual_orth(double &onrm);
-    
-    //!
-    //! \fn compute_metrics(double rnrm, double onrm, double anrm, double xnrm, 
-    //!     double bnrm)
-    //! \brief Compute the different metrics
-    //!
-    //! This method will compute all the different metrics:
-    //!     - norm of A
-    //!     - norm of b
-    //!     - norm of the solution
-    //!     - norm of the residual r=Ax-b
-    //!     - norm of the orthogonal residual: ||A'r||/||r||
-    //!
-    //! \param onrm: norm of the orthogonal residual
-    //!
-    void compute_metrics(double &rnrm, double &onrm, double &anrm, double &xnrm, 
-        double &bnrm);
+//public:
+//    //!
+//    //! \brief Constructor of the Metrics class
+//    //!
+//    //! \return the Metrics instance
+//    //!
+//    Metrics();
+//    
+//    //!
+//    //! \brief Destructor of the Metrics class
+//    //!
+//    ~Metrics();
+//    
+//    //!
+//    //! \fn void init_metrics(int m, int n, int nz, double *values, int *irn,
+//    //! int*jcn, double *x, double *r)
+//    //! \brief Initialize the data (QR_Mumps structure, residual and solution)
+//    //!
+//    //! \param m, n, nz: number of rows, columns and non-zero values
+//    //! \param values, irn/jcn: arrays of non-zero values, row/column indices
+//    //! \param x: solution array
+//    //! \param r: residual array (Ax-b)
+//    //!
+//    void init_metrics(int m, int n, int nz, double *values, int *irn,
+//        int*jcn, double *x, double *r);
+//    
+//    //!
+//    //! \fn A_norm(double &anrm)
+//    //! \brief Compute the norm of the matrix A
+//    //!
+//    //! \param anrm: norm of A
+//    //!
+//    void A_norm(double &anrm);
+//    
+//    //!
+//    //! \fn b_norm(double &bnrm)
+//    //! \brief Compute the norm of the right hand side b
+//    //!
+//    //! \param bnrm: norm of the right hand side b
+//    //!
+//    void b_norm(double &bnrm);
+//    
+//    //!
+//    //! \fn sol_norm(double &xnrm)
+//    //! \brief Compute the norm of the solution x
+//    //!
+//    //! \param xnrm: norm of the solution x
+//    //!
+//    void sol_norm(double &xnrm);
+//    
+//    //!
+//    //! \fn r_norm(double &rnrm)
+//    //! \brief Compute the norm of the residual r=Ax-b
+//    //!
+//    //! \param rnrm: norm of the residual
+//    //!
+//    void residual_norm(double &rnrm);
+//    
+//    //!
+//    //! \fn o_norm(double &onrm)
+//    //! \brief Compute the norm of the orthogonal residual onrm=||A'r||/||r||
+//    //!
+//    //! \param onrm: norm of the orthogonal residual
+//    //!
+//    void residual_orth(double &onrm);
+//    
+//    //!
+//    //! \fn compute_metrics(double rnrm, double onrm, double anrm, double xnrm, 
+//    //!     double bnrm)
+//    //! \brief Compute the different metrics
+//    //!
+//    //! This method will compute all the different metrics:
+//    //!     - norm of A
+//    //!     - norm of b
+//    //!     - norm of the solution
+//    //!     - norm of the residual r=Ax-b
+//    //!     - norm of the orthogonal residual: ||A'r||/||r||
+//    //!
+//    //! \param onrm: norm of the orthogonal residual
+//    //!
+//    void compute_metrics(double &rnrm, double &onrm, double &anrm, double &xnrm, 
+//        double &bnrm);
 };
 
 #endif /* METRICS_H */
